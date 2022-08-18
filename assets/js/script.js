5fd739c03f68a972b9ec92ff58ac6b2654d92494
Christopher DiStasio <cadistasio@gmail.com>
2022-08-18 11:40:56 -0500

    Clears scoreboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,60 @@
-// TODO: Declare any global variables we need
-let heads_count =0;
-let tails_count = 0;
-let heads_percent = 0;
+let heads_count=0;
+let tails_count=0;
+let heads_percent=0;
 let tails_percent=0;
 let total=0;
 
+let headsCell = document.getElementById("heads")
+
+let headsPercent = document.getElementById("heads-percent")
+
+let tailsCell = document.getElementById("tails")
+
+let tailsPercent = document.getElementById("tails-percent")
 
 let image = document.getElementById("image").src = 'assets/images/penny-heads.jpg'
 
-let message = message
+        // TODO: Update status message in the DOM
 
+        document.getElementById("clear").addEventListener("click", function(){
 
-    // TODO: Add event listener and handler for flip and clear buttons
+          image=document.getElementById('image').src = 'assets/images/penny-heads.jpg';
 
+          headsCell.textContent = 0
 
+          headsPercent.textContent = 0
 
-        // TODO: Update image and status message in the DOM
+          tailsCell.textContent = 0
 
-    document.getElementById("flip").addEventListener("click", function(){
+          tailsPercent.textContent = 0
 
-        let flip = Math.round((Math.random() * 10))
+          heads_count=0;
+          tails_count=0;
+          heads_percent=0;
+          tails_percent=0;
+          total=0;
+        })
 
-        if(flip <= 5){
-            tails_count++
-            total++
-            image=document.getElementById('image').src = 'assets/images/penny-tails.jpg';
-        } else if(flip>5) {
-            heads_count++
-            total++
-            image=document.getElementById('image').src = 'assets/images/penny-heads.jpg';
-        }
+        document.getElementById("flip").addEventListener("click", function(){
 
-        let headsCell = document.getElementById("heads")
+          let flip = Math.round((Math.random() * 10))
 
-        headsCell.textContent = heads_count
-
-
-        let headsPercent = document.getElementById("heads-percent")
-
-        headsPercent.textContent = heads_percent
-
-
-        let tailsCell = document.getElementById("tails")
-
-        tailsCell.textContent = tails_count
-
-
-        let tailsPercent = document.getElementById("tails-percent")
-
-        tailsPercent.textContent = tails_percent
-
+          if(flip <= 5){
+              tails_count++
+              total++
+              tailsCell.textContent = tails_count
+              image=document.getElementById('image').src = 'assets/images/penny-tails.jpg';
+          } else if(flip>5) {
+              heads_count++
+              total++
+              headsCell.textContent = heads_count
+              image=document.getElementById('image').src = 'assets/images/penny-heads.jpg';
+          }
     })
 
 
 
-
-
-
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
         // TODO: Use the calculated total to calculate the percentages
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
